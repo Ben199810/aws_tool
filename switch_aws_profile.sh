de6b7b47e56168dc541c736a30a54cfa41d171e1
Ben199810 <ben29774583@gmail.com>
2025-01-17 16:22:00 +0800

    update: 更新說明 & 指令執行排序

--- switch_aws_profile.sh
+++ switch_aws_profile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# 執行此腳本請使用 source 指令
+
 # 字體顏色
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
+
+CREDENTIALS_FILE=~/.aws/credentials
 
 accounts_list=($(aws configure list-profiles))
 
@@ -20,8 +24,8 @@ select selected_account in "${accounts_list[@]}"; do
         if [[ -n "$selected_account" && "$selected_account" == "$current_account" ]]; then
           echo -e "${YELLOW}Already using this account.${NC}"
         elif [ -n "$selected_account" ]; then
-          export AWS_PROFILE=${selected_account}
           echo -e "${BLUE}Selected AWS Account: $selected_account${NC}"
+          export AWS_PROFILE=${selected_account}
           echo -e "${GREEN}Switched to AWS profile: $AWS_PROFILE${NC}"
           aws sts get-caller-identity --no-cli-pager
         else
